7637ff5774cbed9a9a513d18ff6240beb2d90644
Chandraaditya <chandraaditya2001@gmail.com>
2020-08-11 04:19:50 +0530

    Login and Signup complete

--- models/user/user.go
+++ models/user/user.go
@@ -7,12 +7,14 @@ import (
 	"crypto/rand"
 	"errors"
 	"log"
+	mathRand "math/rand"
+	"time"
 )
 
 type User struct {
 	ID        int
 	Email     string
-	Username  string
+	Name      string
 	PlainPass string
 	HashPass  string
 	HouseNo   string
@@ -24,22 +26,25 @@ func (user User) Authenticate() (err error) {
 		log.Println(err.Error())
 		return
 	}
-	stmt, err := db.Conn.Prepare(`SELECT id, username, password FROM users WHERE email=?`)
+	stmt, err := db.Conn.Prepare(`SELECT id, name, password FROM users WHERE email=?`)
 	if err != nil {
 		log.Println("el1002: error connecting to db:", err)
 		return
 	}
 	defer stmt.Close()
-	err = stmt.QueryRow(user.Email).Scan(&user.ID, &user.Username, &user.HashPass)
-	if err.Error() == "sql: no rows in result set" {
-		err = errors.New("el1004")
-		return
+	err = stmt.QueryRow(user.Email).Scan(&user.ID, &user.Name, &user.HashPass)
+	if err != nil {
+		if err.Error() == "sql: no rows in result set" {
+			err = errors.New("el1004")
+			return
+		}
 	}
 	if err != nil {
 		log.Println("el1003: error querying from db:", err)
 		return
 	}
 	err = Verify(user.PlainPass, user.HashPass)
+	user.PlainPass = ""
 	if err != nil {
 		err = errors.New("ea1001: error authenticating user")
 		return
@@ -48,11 +53,88 @@ func (user User) Authenticate() (err error) {
 }
 
 func (user User) CreateNewUser() (err error) {
+	err = db.Err
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	stmt, err := db.Conn.Prepare(`INSERT INTO users(id, email, name, password, houseno) VALUES (?, ?, ?, ?, ?)`)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	user.ID, err = GenerateUserID()
+	if err != nil {
+		if err.Error() != "eu1002" {
+			log.Println(err.Error())
+			return
+		}
+	}
+	user.HashPass, err = Hash(user.PlainPass)
+	user.PlainPass = ""
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	res, err := stmt.Exec(user.ID, user.Email, user.Name, user.HashPass, user.HouseNo)
+	if err != nil {
+		log.Println(err.Error())
+		if err.Error()[:10] == "Error 1062" {
+			err = errors.New("ea1001")
+			log.Println(err.Error())
+		}
+		return
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+	return
+}
+
+func CheckGeneratedIDExits(ID int) (err error) {
+	var ExistingID int
+	stmt, err := db.Conn.Prepare(`SELECT id FROM users WHERE id=?`)
+	if err != nil {
+		err = errors.New("eu1001")
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(ID).Scan(&ExistingID)
+	if err != nil {
+		err = errors.New("eu1002")
+		return
+	}
+	err = errors.New("eu1003")
+	return
+}
+
+func GenerateUserID() (ID int, err error) {
+	min := 100000000
+	max := 999999999
+	mathRand.Seed(time.Now().UnixNano())
+	for {
+		ID = mathRand.Intn(max-min+1) + min
+		err = CheckGeneratedIDExits(ID)
+		if err.Error() == "eu1001" {
+			return
+		}
+		if err.Error() == "eu1002" {
+			log.Println("Generated user ID:", ID)
+			break
+		}
+	}
 	return
 }
 
 func Verify(Plain, Hash string) (err error) {
-	//TODO need to finish hash function
 	cp, err1 := base64.Decode([]byte(Hash))
 	if err1 != nil {
 		log.Println(err.Error())
